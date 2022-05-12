f9d146af844f4da8d4716f01c5e274322ddf9e6c
weripes <panasicoveduard@gmail.com>
2022-05-12 17:48:17 +0300

    added base_dir

--- src/cookie_utils.py
+++ src/cookie_utils.py
@@ -2,6 +2,7 @@ import pickle
 import os
 
 from .settings.logger import logger
+from .settings.base_dir import BASE_DIR
 
 
 class CookieUtils:
@@ -10,21 +11,21 @@ class CookieUtils:
         self.driver = driver
 
     def dump_cookies(self, filename: str) -> None:
-        if not os.path.exists('cookies'):
-            os.mkdir('cookies')
+        if not os.path.exists(f'{BASE_DIR}/src/cookies'):
+            os.mkdir(f'{BASE_DIR}/src/cookies')
 
-        pickle.dump(self.driver.get_cookies() , open(f'cookies/{filename}.pkl', 'wb'))
-        logger.info(f'Cookies saved to file {filename}.pkl in path /cookies')
+        pickle.dump(self.driver.get_cookies() , open(f'{BASE_DIR}/src/cookies/{filename}.pkl', 'wb'))
+        logger.info(f'Cookies saved to file {filename}.pkl')
 
     def load_cookies(self, filename: str) -> None:
 
-        if os.path.isfile(f'cookies/{filename}.pkl'):
-            cookies = pickle.load(open(f'cookies/{filename}.pkl', 'rb'))
+        if os.path.isfile(f'{BASE_DIR}/src/cookies/{filename}.pkl'):
+            cookies = pickle.load(open(f'{BASE_DIR}/src/cookies/{filename}.pkl', 'rb'))
 
             for cookie in cookies:
                 self.driver.add_cookie(cookie)
-            logger.info(f'Cookies loaded (filename={filename}.pkl, path /cookies)')
+            logger.info(f'Cookies loaded (filename={filename}.pkl')
 
         else:
-            logger.error(f'File {filename}.pkl does not found in path /cookies')
-            raise FileNotFoundError(f'File {filename}.pkl does not found in path /cookies')
+            logger.error(f'File {filename}.pkl does not found')
+            raise FileNotFoundError(f'File {filename}.pkl does not found')
